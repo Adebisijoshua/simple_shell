c3c5a30ecdbe7349d4e9db70adce88c8dfbed071
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-14 10:37:44 -0800

    When will the checker be released

--- Maleka_built.c
+++ Maleka_built.c
@@ -1,21 +1,21 @@
 #include "builtins.h"
 
 /**
- * get_builtins - get the builtins
- * Return: pointer to a NULL-terminated statically-allocated array of builtins
+ * get_builtins - get builtins
+ * Return: NULL-terminated statically-allocated array of builtins
  */
 const builtin_t *get_builtins(void)
 {
 	static builtin_t builtins[] = {
 		{"alias", __alias, ALIAS_HELP, ALIAS_DESC},
 		{"cd", __cd, CD_HELP, CD_DESC},
-		{"env", __env, ENV_HELP, ENV_DESC},
 		{"exec", __exec, EXEC_HELP, EXEC_DESC},
+		{"setenv", __setenv, SETENV_HELP, SETENV_DESC},
 		{"exit", __exit, EXIT_HELP, EXIT_DESC},
 		{"help", __help, HELP_HELP, HELP_DESC},
-		{"setenv", __setenv, SETENV_HELP, SETENV_DESC},
+		{"env", __env, ENV_HELP, ENV_DESC},
 		{"unsetenv", __unsetenv, UNSETENV_HELP, UNSETENV_DESC},
-		{0}
+		{0} /* Null-terminate the array */
 	};
 
 	return (builtins);
@@ -23,17 +23,18 @@ const builtin_t *get_builtins(void)
 
 /**
  * get_builtin - get a builtin by name
- * @name: the name of the builtin to retrieve
- * Return: NULL if no match is found, otherwise a pointer to the builtin
+ * @name: the name of the builtin that will be retrieved
+ * Return: if no match is found NULL, otherwise a pointer to the builtin
  */
 const builtin_t *get_builtin(const char *name)
 {
 	const builtin_t *builtin = NULL;
 
+	/* Iterate through the array of built-in commands */
 	for (builtin = get_builtins(); builtin->name; builtin += 1)
 	{
 		if (_strcmp(name, builtin->name) == 0)
-			return (builtin);
+			return (builtin);/* Return the matching builtin if found */
 	}
-	return (NULL);
+	return (NULL);/* Return NULL if no match is found */
 }
--- cmd_to_list.c
+++ cmd_to_list.c
@@ -1,10 +1,10 @@
 #include "command.h"
 
 /**
- * cmd_to_list - construct a linked list of tokenized commands
+ * cmd_to_list - build a linked list of token for commands
  * @cmd: the command to parse
  *
- * Return: If memory allocation fails, return NULL. Otherwise, return a
+ * Return: On fails, give  NULL. Otherwise, return 1
  * pointer to the head of the new list.
  */
 cmdlist_t *cmd_to_list(const char *cmd)
@@ -13,11 +13,13 @@ cmdlist_t *cmd_to_list(const char *cmd)
 	size_t count;
 	char *split = _strdup(cmd);
 
+	/* Check for memory allocation failure */
 	if (!split)
 		return (NULL);
 
 	count = split_cmd(split);
 
+	/* Check for memory allocation failure during list construction */
 	if (!_cmd_to_list(&head, split, count))
 	{
 		free_cmdlist(&head);
@@ -25,32 +27,37 @@ cmdlist_t *cmd_to_list(const char *cmd)
 	}
 	free(split);
 
-	return (head);
+	return (head);/* return */
 }
 
 
 /**
- * _cmd_to_list - construct a linked list of tokenized commands (helper)
- * @tailptr: pointer to the tail of the command list
+ * _cmd_to_list - build a linked list of tokenized commands
+ * @tailptr: pointer to the tail at the command list
  * @split: a line split with null bytes on separators
- * @count: the number of commands contained in split
+ * @count: Total number of commands we have in the list
  *
- * Return: If memory allocation fails, return NULL. Otherwise, return a
- * pointer to the tail of the new list.
+ * Return: On fails,give  return NULL. Otherwise, return 1
+ * pointer to the tail at the new list.
  */
 cmdlist_t *_cmd_to_list(cmdlist_t **tailptr, char *split, size_t count)
 {
 	cmdlist_t *tail;
 
+	/* If count is zero, return the current tail pointer */
 	if (!count)
 		return (*tailptr);
 
+	/* Add a new command to the end of the list and update the tail pointer */
 	tail = add_cmd_end(tailptr, split);
+	/* Check for memory allocation failure */
 	if (!tail)
 		return (NULL);
 
+	/* Move the split pointer to the next command */
 	while (*split++)
 		;
 
+	/* Recursively call _cmd_to_list for the remaining commands */
 	return (_cmd_to_list(&tail, split, count - 1));
 }
