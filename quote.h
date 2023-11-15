97923337df9078ca7ee2a6ec023ae717c970e42d
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-15 10:16:49 -0800

    This code should

--- quote.h
+++ quote.h
@@ -29,16 +29,17 @@ quote_state_t quote_state(char c);
 
 quote_state_fp quote_state_f(quote_state_t s);
 
-size_t quote_state_len(const char *str, quote_state_t state);
+
 size_t _quote_state_none(const char *str, quote_state_t *state);
 size_t _quote_state_word(const char *str, quote_state_t *state);
+size_t quote_state_len(const char *str, quote_state_t state);
+size_t _quote_state_escape(const char *str, quote_state_t *state);
 size_t _quote_state_double(const char *str, quote_state_t *state);
 size_t _quote_state_single(const char *str, quote_state_t *state);
-size_t _quote_state_escape(const char *str, quote_state_t *state);
 
 int _isspecial_double(char c);
 
 char *dequote(const char *str);
 size_t dequote_len(const char *str);
 
-#endif /* _QUOTE_H_ */
+#endif
