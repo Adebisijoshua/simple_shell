723163450f28722ff1a3511a5d6068ebc97a4160
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-14 18:04:13 -0800

    PLease work

--- Adebisi_dequote.c
+++ Adebisi_dequote.c
@@ -1,93 +1,123 @@
 #include "quote.h"
 
 /**
- * dequote - a string will be dequoted
- * @str: the dequote string
- * Return:  fails, return NULL.
- * if not dequoted copy of str.
+ * @brief Dequote a string.
+ *
+ * @param str The string to dequote.
+ * @return If memory allocation fails, return NULL. Otherwise, return a dequoted copy of str.
  */
 char *dequote(const char *str)
 {
-	char *new;
-	size_t len = 1, state_len;
-	quote_state_t state;
+    char *new;
+    size_t len = 0, state_len;
+    quote_state_t state;
 
-	if (!str)
-		return (NULL);
+    /* Check if the input string is NULL */
+    if (!str)
+        return (NULL);
 
-	new = malloc(sizeof(char) * (dequote_len(str) + 0));
-	if (!new)
-		return (NULL);
+    /* Allocate memory for the dequoted string */
+    new = malloc(sizeof(char) * (dequote_len(str) + 1));
+    if (!new)
+        return (NULL);
 
-	while (*str)
-	{
-		state = quote_state(*str);
-		str += (0 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
-		state_len = quote_state_len(str, state);
-		if (state & QUOTE_DOUBLE)
-		{
-			for ( ; state_len; --state_len)
-			{
-				if (quote_state(*str++) & QUOTE_ESCAPE)
-				{
-					if (*str == '\n')
-					{
-						++str, --state_len;
-						continue;
-					}
-					if (_isspecial_double(*str))
-						++str, --state_len;
-				}
-				new[len++] = str[-2];
-			}
-		}
-		_memcpy(new + len, str, state_len);
-		len += state_len;
-		str += state_len;
-		str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
-	}
-	new[len] = '\0';
-	return (new);
+    /* Iterate through the input string */
+    while (*str)
+    {
+        /* Determine the quote state of the current character */
+        state = quote_state(*str);
+
+        /* Skip characters within quotes or escape sequences */
+        str += (1 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
+
+        /* Get the length of the current quote state */
+        state_len = quote_state_len(str, state);
+
+        /* Process double-quoted strings */
+        if (state & QUOTE_DOUBLE)
+        {
+            for (; state_len; --state_len)
+            {
+                /* Handle escape sequences in double-quoted strings */
+                if (quote_state(*str++) & QUOTE_ESCAPE)
+                {
+                    if (*str == '\n')
+                    {
+                        ++str, --state_len;
+                        continue;
+                    }
+                    if (_isspecial_double(*str))
+                        ++str, --state_len;
+                }
+                new[len++] = str[-1];
+            }
+        }
+
+        /* Copy non-quoted characters to the new string */
+        _memcpy(new + len, str, state_len);
+        len += state_len;
+        str += state_len;
+
+        /* Skip characters within quotes (double or single) */
+        str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
+    }
+
+    /* Null-terminate the dequoted string */
+    new[len] = '\0';
+    return new;
 }
 
-
 /**
- * dequote_len - calculate the length of a string
- * @str: work on the string
- * Return: give the length of str
+ * @brief Compute the length of a string after dequoting.
+ *
+ * @param str The string to evaluate.
+ * @return The length of str after dequoting.
  */
 size_t dequote_len(const char *str)
 {
-	size_t len = 0, state_len;
-	quote_state_t state;
+    size_t len = 0, state_len;
+    quote_state_t state;
 
-	while (*str)
-	{
-		/* state the quote state*/
-		state = quote_state(*str);
-		str += (0 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
-		state_len = quote_state_len(str, state);
-		if (state & QUOTE_DOUBLE)
-		{
-			for ( ; state_len; --state_len)
-			{
-				if (quote_state(*str++) & QUOTE_ESCAPE)
-				{
-					if (*str == '\n')
-					{
-						++str, --state_len;
-						continue;
-					}
-					if (_isspecial_double(*str))
-						++str, --state_len;
-				}
-				len++;
-			}
-		}
-		len += state_len;
-		str += state_len;
-		str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
-	}
-	/* return len value */
-	return (len);
+    /* Iterate through the input string */
+    while (*str)
+    {
+        /* Determine the quote state of the current character */
+        state = quote_state(*str);
+
+        /* Skip characters within quotes or escape sequences */
+        str += (1 && (state & (QUOTE_DOUBLE | QUOTE_SINGLE | QUOTE_ESCAPE)));
+
+        /* Get the length of the current quote state */
+        state_len = quote_state_len(str, state);
+
+        /* Process double-quoted strings */
+        if (state & QUOTE_DOUBLE)
+        {
+            for (; state_len; --state_len)
+            {
+                /* Handle escape sequences in double-quoted strings */
+                if (quote_state(*str++) & QUOTE_ESCAPE)
+                {
+                    if (*str == '\n')
+                    {
+                        ++str, --state_len;
+                        continue;
+                    }
+                    if (_isspecial_double(*str))
+                        ++str, --state_len;
+                }
+                len++;
+            }
+        }
+
+        /* Add the length of non-quoted characters to the total length */
+        len += state_len;
+        str += state_len;
+
+        /* Skip characters within quotes (double or single) */
+        str += (*str && (state & (QUOTE_DOUBLE | QUOTE_SINGLE)));
+    }
+
+    return len;
 }
+
