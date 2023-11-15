4aca4c65e28000346f2ad075a352d8866aa05517
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-15 09:52:01 -0800

    Alx is not for the weak

--- dict.c
+++ dict.c
@@ -1,30 +1,33 @@
 #include "dict.h"
 
 /**
-  * get_dict_val - get a value from a dictionary
+  * get_dict_val - take a value from a dictionary
   * @head: dict
   * @key: entry key
   *
-  * Return: pointer to the entry value
+  * Return: enttry value to the key
   */
 char *get_dict_val(dict_t *head, const char *key)
 {
+	/* Check if key is NULL */
 	if (!key)
 		return (NULL);
 
+	/* Traverse the dictionary to find the entry with the specified key */
 	while (head)
 	{
+		/* Compare the current node's key with the target key */
 		if (!_strcmp(head->key, key))
-			return (head->val);
-		head = head->next;
+			return (head->val);/* Return the value if the key matches */
+		head = head->next;/* Move to the next node */
 	}
 
-	return (NULL);
+	return (NULL);/* Return NULL if the key is not found */
 }
 
 
 /**
-  * get_dict_node - retrieve a node by key
+  * get_dict_node - get a node by key
   * @head: dict
   * @key: entry key
   *
@@ -32,47 +35,57 @@ char *get_dict_val(dict_t *head, const char *key)
   */
 dict_t *get_dict_node(dict_t *head, const char *key)
 {
+	/* Check if the dictionary is empty */
 	if (!head)
 		return (NULL);
+	/* Compare the key of the current node with the target key */
 	if (!_strcmp(head->key, key))
-		return (head);
+		return (head);/* Return the node if the key matches */
+
+	/* Recursive call to search for the key in the next nodes */
 	return (get_dict_node(head->next, key));
 }
 
 
 /**
-  * add_dict_node_end - adds node at the end of list
-  * @headptr: pointer to dict
-  * @key: entry key
-  * @val: entry val
+  * add_dict_node_end - give node at the end of list
+  * @headptr: pointer
+  * @key: key of the entry
+  * @val: val of the entry
   * Return: pointer to list
   */
 dict_t *add_dict_node_end(dict_t **headptr, const char *key, const char *val)
 {
 	dict_t *new;
 
+	/* Check if the head pointer is NULL */
 	if (!headptr)
 		return (NULL);
 
+	/* If the list is not empty, recursively call to add the node to the end */
 	if (*headptr)
 		return (add_dict_node_end(&((*headptr)->next), key, val));
 
+	/* Allocate memory for the new node */
 	new = malloc(sizeof(dict_t));
 	if (!new)
 		return (NULL);
 
+	/* Copy the key and value to the new node */
 	new->key = _strdup(key);
 	new->val = _strdup(val);
 	new->next = NULL;
 
+	/* Set the head pointer to the new node */
 	*headptr = new;
+	/* Return the pointer to the new node */
 	return (new);
 }
 
 
 /**
-  * del_dict_node - delete a node
-  * @headptr: pointer to dict
+  * del_dict_node - delete
+  * @headptr: pointer
   * @key: entry key
   *
   * Return: pointer to resulting list
@@ -81,10 +94,13 @@ dict_t *del_dict_node(dict_t **headptr, const char *key)
 {
 	dict_t *tmp;
 
+	/* Check if the head pointer or dictionary is empty */
 	if  (!(headptr && *headptr))
 		return (NULL);
+	/* Check if the key matches the key of the current node */
 	if (!_strcmp((*headptr)->key, key))
 	{
+		/* Remove the current node and update the head pointer */
 		tmp = *headptr;
 		(*headptr) = tmp->next;
 		free(tmp->key);
@@ -92,7 +108,10 @@ dict_t *del_dict_node(dict_t **headptr, const char *key)
 		free(tmp);
 	}
 	else
+		/* Recursive call to search for the key in the next nodes */
+
 		del_dict_node(&((*headptr)->next), key);
+	/* Return the updated head pointer */
 	return (*headptr);
 }
 
@@ -103,10 +122,13 @@ dict_t *del_dict_node(dict_t **headptr, const char *key)
  */
 void free_dict(dict_t **headptr)
 {
+	/* Check if the dictionary is empty */
 	if (!*headptr)
 		return;
 
+	/* Recursive call to free the next nodes in the list */
 	free_dict(&((*headptr)->next));
+	/* Free the key, value, and the current node */
 	free((*headptr)->key);
 	free((*headptr)->val);
 	free(*headptr);
