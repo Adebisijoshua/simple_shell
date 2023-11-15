c2d2b55302d2e4e50aec65d3eac6313fbe317938
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-15 09:56:09 -0800

    Team work

--- list.h
+++ list.h
@@ -6,9 +6,9 @@
 #include "types.h"
 
 /**
- * struct list - singly linked list
- * @str: dynamically-allocated string
- * @next: pointer to the next node
+ * struct list - singly linked
+ * @str: dynamically-allocated
+ * @next: pointer
  */
 struct list
 {
@@ -17,9 +17,9 @@ struct list
 };
 
 list_t *str_to_list(const char *str, char delim);
+list_t *add_node_end(list_t **headptr, const char *str);
 list_t *_str_to_list(list_t **tailptr, const char *str, char delim);
 list_t *add_node(list_t **headptr, const char *str);
-list_t *add_node_end(list_t **headptr, const char *str);
 void free_list(list_t **headptr);
 
 #endif
