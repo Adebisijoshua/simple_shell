68b2e3d4e6f7b76ebdae353ba86d0f5542308038
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-15 21:10:55 -0800

    we are at the second dead line

--- env.h
+++ env.h
@@ -2,14 +2,14 @@
 #define _ENV_H_
 
 #include <stdlib.h>
-#include "dict.h"
 #include "string.h"
+#include "dict.h"
 #include "types.h"
 
 typedef dict_t env_t;
-
-env_t *env_to_dict(char **env);
 env_t *_env_to_dict(env_t **tailptr, char **env);
 char **dict_to_env(env_t *head);
+env_t *env_to_dict(char **env);
 
-#endif /* _ENV_H_ */
+
+#endif
--- types.h
+++ types.h
@@ -1,12 +1,13 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
+/* Here goes the prototype */
 typedef struct builtin builtin_t;
-typedef struct cmdtree cmdtree_t;
 typedef struct history history_t;
-typedef struct dict dict_t;
 typedef struct cmdlist cmdlist_t;
+typedef struct dict dict_t;
+typedef struct cmdtree cmdtree_t;
 typedef struct info info_t;
 typedef struct list list_t;
 
-#endif
+#endif /* TYPES H FILES */
