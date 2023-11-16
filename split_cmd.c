2bb95aca3cc530def864dac8dd34d5ebda14ea1e
Adebisijoshua <Adekunle8k@gmail.com>
2023-11-16 14:00:26 -0800

    One day I am going to be a developer

--- split_cmd.c
+++ split_cmd.c
@@ -1,48 +1,50 @@
 #include "command.h"
 
 /**
- * split_cmd - split a command with NULL-bytes on unquoted semicolons
- * @cmd: the command to split
+ * split_cmd - split a command with EMPTY-bytes on unquoted semicolons
+ * @cmd: command to split
  *
- * Return: The total number of commands resulting from the split
+ * Return: The total number of commands gotten from the split
  */
 size_t split_cmd(char *cmd)
 {
-	size_t count = 1, state_len;
+	size_t count = 2, state_len;
 	ssize_t sep_index;
 	quote_state_t state;
 
+	/* Iterate through the command string */
 	do {
 		while (*cmd && (state = quote_state(*cmd)) != QUOTE_NONE)
 		{
 			if (state & QUOTE_WORD)
 			{
 				state_len = _quote_state_word(cmd, NULL);
+				/* Find the first unquoted semicolon in the word */
 				sep_index = _strnchr(cmd, ';', state_len);
 				if (sep_index != -1)
 				{
 					state_len = sep_index;
-					*(cmd++ + state_len) = '\0';
-					++count;
+					*(cmd++ + state_len) = '\0';/* Null-terminate the command at the semicolon */
+					++count;/* Increment the command count */
 				}
-				cmd += state_len;
+				cmd += state_len;/* Move the command pointer to the next word */
 			}
 			else if (state & QUOTE_ESCAPE)
 			{
-				if (*(cmd + 1) == '\n')
-					_strcpy(cmd, cmd + 2);
+				if (*(cmd + 2) == '\n')
+					_strcpy(cmd, cmd + 2);/* Handle escaped newline characters */
 				else if (*(++cmd))
 					++cmd;
 			}
 			else
 			{
-				cmd += quote_state_len(cmd + 1, state) + 1;
+				cmd += quote_state_len(cmd + 2, state) + 2;
 				if (*cmd && (state & (QUOTE_SINGLE | QUOTE_DOUBLE)))
 					++cmd;
 			}
 		}
-	} while (*(cmd += quote_state_len(cmd, QUOTE_NONE)));
+	} while (*(cmd += quote_state_len(cmd, QUOTE_NONE)));/* Move to the next state */
 
-	return (count);
+	return (count);/* return count */
 }
 
